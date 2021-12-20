cefd3114bf24259ceda6f71206a8b24b1ca2f2f4
Mattia Maio <mattia.maio10@gmail.com>
2021-12-20 13:30:29 +0100

    bonus fatto

--- src/main/java/org/generation/italy/Main.java
+++ src/main/java/org/generation/italy/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Inserire parametro di ricerca: ");
+		System.out.print("Insert research parameter: ");
 		String filter = scan.nextLine();
 
 		try (Connection con = DriverManager.getConnection(URL, USER, PW)) {
@@ -38,9 +38,53 @@ public class Main {
 				}
 
 			}
+			System.out.print("Select country id: ");
+			int countryId = scan.nextInt();
+
+			String query = "SELECT c.name\n" + "FROM countries c \n" + "WHERE c.country_id = ?; \n" + "";
+
+			try (PreparedStatement ps = con.prepareStatement(query)) {
+				ps.setInt(1, countryId);
+				try (ResultSet rs = ps.executeQuery()) {
+					while (rs.next()) {
+						System.out.println("Details for country: " + rs.getString(1));
+					}
+				}
+			}
+
+			String query2 = "SELECT c.name , l.`language`\n" + "FROM languages l\n" + "JOIN country_languages cl\n"
+					+ "ON cl.language_id = l.language_id\n" + "JOIN countries c\n" + "ON cl.country_id = c.country_id\n"
+					+ "WHERE c.country_id = ?;";
+
+			try (PreparedStatement ps = con.prepareStatement(query2)) {
+				ps.setInt(1, countryId);
+
+				try (ResultSet rs = ps.executeQuery()) {
+					System.out.print("Languages: ");
+					while (rs.next()) {
+						System.out.print(rs.getString(2) + " ");
+					}
+				}
+			}
+
+			String query3 = "SELECT cs.`year`, cs.population, cs.gdp \n" + "FROM country_stats cs \n"
+					+ "JOIN countries c \n" + "ON c.country_id = cs.country_id \n" + "WHERE c.country_id = ? \n"
+					+ "ORDER BY `year` DESC\n" + "LIMIT 1;";
+
+			try (PreparedStatement ps = con.prepareStatement(query3)) {
+				ps.setInt(1, countryId);
+				try (ResultSet rs = ps.executeQuery()) {
+					System.out.println("\nMost recent stats");
+					while (rs.next()) {
+						System.out.println("Year: " + rs.getString(1));
+						System.out.println("Population: " + rs.getString(2));
+						System.out.println("GDP: " + rs.getString(3));
+					}
+				}
+			}
 
 		} catch (SQLException e) {
-			System.out.println("Errore");
+			System.out.println("Error");
 			System.out.println(e.getMessage());
 		}
 
